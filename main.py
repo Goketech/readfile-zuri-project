2c3dc38408e4d2c6cb8f98af01f6f3700fba0065
Akanni Modupe Adegoke <105242931+Goketech@users.noreply.github.com>
2022-05-27 15:28:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def get_score(self):
+        return self.score
+    def change_name(self, name):
+        self.name = name
+    def change_age(self, age):
+        self.age = age
+    def add_track(self, tracks):
+        self.tracks = tracks
+
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+
+print(Bob.name)
+print(Bob.get_score())
+print(Bob.age)
+print(Bob.tracks)
